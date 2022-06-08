7c2fd7ae7a23356e7ef0a95d5b3901731ddf2c0a
zakeeee <zakeeee@163.com>
2022-06-08 23:37:43 +0800

    refactor: rename component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,9 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <HelloWorld />
+  <DragFileToUpload />
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
-</style>
+<script setup lang="ts">
+import DragFileToUpload from './components/DragFileToUpload.vue';
+</script>
+
+<style></style>
